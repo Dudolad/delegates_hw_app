38bbe0ea0b18c5d3e7d128d0d994f9d9d0f109eb
dudolad_yaroslav <dudolad2009@gmail.com>
2026-01-17 23:32:59 +0200

    Add project files.

---
+++ delegates_hw_app/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace delegates_hw_app
+{
+    public delegate double Calc(double a, double b);
+    public class Program
+    {
+        public static double Add(double a, double b) => a + b;
+        public static double Subtract(double a, double b) => a - b;
+        public static double Multiply(double a, double b) => a * b;
+        public static double Divide(double a, double b) => (b != 0) ? a / b :
+            throw new Exception("Divide by zero...");
+        static void Main(string[] args)
+        {
+            Calc calc = new Calc(Subtract);
+            CalculatorDisplay(Add, 14, 10);
+            CalculatorDisplay(Subtract, 14, 10);
+            CalculatorDisplay(Divide, 14, 10);
+            CalculatorDisplay(Multiply, 14, 10);
+            Console.ReadKey();
+        }
+        public static void CalculatorDisplay(Calc calc, double a, double b) =>
+            Console.WriteLine($"Result: {calc(a, b)}");
+    }
+}
