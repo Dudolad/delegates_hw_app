10f973e163f8d4d68bd0f5dc601db2c8c3caead7
dudolad_yaroslav <dudolad2009@gmail.com>
2026-01-17 23:49:31 +0200

    Realized task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace Task3
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> strings = new List<string> { "hello", "world", "Yaroslaves", "delegate" };
+
+            Action<string> printUpper = s => Console.WriteLine("Upper: " + s.ToUpper());
+            Action<string> printLength = s => Console.WriteLine("Length: " + s.Length);
+            Action<string> addStars = s => Console.WriteLine("Stars: *" + s + "*");
+
+            Action<string> combinedAction = printUpper;
+            combinedAction += printLength;
+            combinedAction += addStars;
+
+            foreach (string str in strings)
+            {
+                Console.WriteLine("Result for string: " + str);
+                combinedAction(str);
+                Console.WriteLine(); 
+            }
+
+            Console.ReadKey();
         }
     }
 }
