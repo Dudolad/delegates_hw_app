c665cc66c4117f046bf4848bc4f4862c0647e1d2
dudolad_yaroslav <dudolad2009@gmail.com>
2026-01-17 23:54:01 +0200

    Realized task4

---
+++ Task4/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Task4
+{
+    public class Program
+    {
+        public static List<int> TransformNumbers(List<int> numbers, Func<int, int> operation)
+        {
+            List<int> result = new List<int>();
+            foreach (int number in numbers)
+            {
+                result.Add(operation(number));
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 1; i <= 20; i++)
+                numbers.Add(i);
+
+            Func<int, int> square = n => n * n;
+            Func<int, int> multiplyBy2 = n => n * 2;
+            Func<int, int> subtract5 = n => n - 5;
+
+            List<int> squaredNumbers = TransformNumbers(numbers, square);
+            List<int> doubledNumbers = TransformNumbers(numbers, multiplyBy2);
+            List<int> subtractedNumbers = TransformNumbers(numbers, subtract5);
+
+            Console.Write("Squared numbers: ");
+            foreach (int n in squaredNumbers)
+                Console.Write(n + " ");
+            Console.WriteLine();
+
+            Console.Write("Doubled numbers: ");
+            foreach (int n in doubledNumbers)
+                Console.Write(n + " ");
+            Console.WriteLine();
+
+            Console.Write("Subtracted 5: ");
+            foreach (int n in subtractedNumbers)
+                Console.Write(n + " ");
+            Console.WriteLine(); Console.ReadKey();
+        }
+    }
+}
